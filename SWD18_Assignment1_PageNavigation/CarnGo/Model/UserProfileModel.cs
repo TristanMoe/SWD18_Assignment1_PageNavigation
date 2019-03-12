4302f2cdb0ea6a04a0e9f496453dc56c27663359
SuperWeebTanks <trilleo12@live.dk>
2019-03-12 11:10:15 +0100

    Added ViewModel and Model to LoginView
    Added ViewModel and Model to LoginView

---
+++ SWD18_Assignment1_PageNavigation/CarnGo/Model/UserProfileModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarnGo.Model
+{
+    public class UserProfileModel
+    {
+        #region Constructors
+        UserProfileModel()
+        {
+            UserName = "";
+            Password = ""; 
+        }
+
+        UserProfileModel(string username, string password)
+        {
+            UserName = username;
+            Password = password; 
+        }
+        #endregion
+
+        #region DummyProperties
+        private string _password; 
+        private string _username; 
+        public string UserName { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
+        #endregion
+    }
+}
---
+++ SWD18_Assignment1_PageNavigation/CarnGo/ViewModel/LoginViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CarnGo.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarnGo.ViewModel
+{
+    public class LoginViewModel
+    {
+        #region Constructors
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Properties
+        private List<UserProfileModel> _userProfiles; 
+        public List<UserProfileModel> UserProfiles
+        {
+            get { return _userProfiles; }
+            set { _userProfiles = value;  }
+        }
+        #endregion
+
+    }
+}
